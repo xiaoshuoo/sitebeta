15fb66bb76732956e0fdc8f0815103dc9711d37f
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 12:05:54 +0300

    commit313ж331

--- mysite/app.py
+++ mysite/app.py
@@ -1,5 +1,11 @@
 import os
+import sys
+
+# Добавляем текущую директорию в PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 app = get_wsgi_application() 
