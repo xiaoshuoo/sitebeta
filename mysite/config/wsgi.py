aa9ea6d2af680115d57f7d34567e64214e77de6e
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 17:32:30 +0300

    Removeee1114э1д1;

--- mysite/config/wsgi.py
+++ mysite/config/wsgi.py
@@ -8,7 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+
+# Добавляем корневую директорию проекта в PYTHONPATH
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 application = get_wsgi_application()
