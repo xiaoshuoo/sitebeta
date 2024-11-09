9ba1b31827a900bc875fa2f000b448a6d8572047
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 10:18:37 +0300

    first1

--- mysite/build.sh
+++ mysite/build.sh
@@ -2,8 +2,16 @@
 # exit on error
 set -o errexit
 
+# Создаем необходимые директории
+mkdir -p staticfiles
+mkdir -p media
+
+# Обновляем pip и устанавливаем зависимости
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+# Собираем статические файлы
 python manage.py collectstatic --no-input
-python manage.py migrate 
+
+# Применяем миграции
+python manage.py migrate
