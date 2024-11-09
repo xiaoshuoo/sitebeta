68e8cff222b5d452568490615b5440555f736bb4
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 16:22:07 +0300

    Remove unnecessary files1111

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,10 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
+# Добавляем путь к проекту в PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 application = get_wsgi_application() 
