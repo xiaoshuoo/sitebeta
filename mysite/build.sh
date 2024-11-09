a1a721b36c9609310235797c80d7c9d87fcf807f
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 12:12:01 +0300

    commit313ж33115

--- mysite/build.sh
+++ mysite/build.sh
@@ -2,11 +2,8 @@
 # exit on error
 set -o errexit
 
-# Install dependencies
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate
