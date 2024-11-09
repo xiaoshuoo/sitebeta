008d051cfcbf2d81eea3311629df5bd4c1c9d8b4
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 10:53:47 +0300

    Normalize line endings1

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='blog:home', permanent=False)),
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
@@ -18,3 +18,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
