332f0e1b0f7f921d892c8704037bc8d6f47ff9ec
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 10:56:58 +0300

    ~~~

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -6,9 +6,8 @@ from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/blog/', permanent=True)),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         next_page='blog:home',
@@ -19,3 +18,8 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    urlpatterns += [
+        path('static/<path:path>', RedirectView.as_view(url=settings.STATIC_URL + '%(path)s')),
+        path('media/<path:path>', RedirectView.as_view(url=settings.MEDIA_URL + '%(path)s')),
+    ]
