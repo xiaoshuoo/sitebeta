1e89afc8a941c3b8cdbe21259e03e022432ebc8b
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 12:20:47 +0300

    Remove unnecessary files

--- mysite/gunicorn.conf.py
+++ mysite/gunicorn.conf.py
@@ -1,9 +1,19 @@
+# Gunicorn configuration file
 import multiprocessing
+
+max_requests = 1000
+max_requests_jitter = 50
+
+log_file = "-"
+log_level = "debug"
 
 bind = "0.0.0.0:$PORT"
 workers = multiprocessing.cpu_count() * 2 + 1
-threads = 2
-worker_class = 'sync'
-worker_connections = 1000
+
 timeout = 120
-keepalive = 2 
+keepalive = 5
+
+capture_output = True
+enable_stdio_inheritance = True
+
+preload_app = True 
--- mysite/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/render/project/src
-exec gunicorn --bind 0.0.0.0:$PORT config.wsgi:application 
