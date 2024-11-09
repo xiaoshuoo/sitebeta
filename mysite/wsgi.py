a91b384ed91c99107c71199e562575140a8914b1
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 17:58:37 +0300

    12345

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -5,14 +5,13 @@ from pathlib import Path
 # Получаем абсолютный путь к директории проекта
 BASE_DIR = Path(__file__).resolve().parent
 
-# Добавляем пути в sys.path
-sys.path.insert(0, str(BASE_DIR))
-
 # Выводим отладочную информацию
 print(f"Current directory: {os.getcwd()}")
 print(f"BASE_DIR: {BASE_DIR}")
-print(f"Python path: {sys.path}")
 print(f"Directory contents: {os.listdir('.')}")
+
+# Добавляем пути в sys.path
+sys.path.insert(0, str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
