896e5d140b01a9509c7df1e872aed81500814117
Xiachoo <odinochkamoto@mail.ru>
2024-11-09 16:37:47 +0300

    Removee;

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -2,8 +2,10 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-# Добавляем путь к проекту в PYTHONPATH
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# Добавляем пути к проекту в PYTHONPATH
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.extend([current_dir, parent_dir])
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
