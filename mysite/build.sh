3fff40a1afeab1aeec353821e26d362fc59d6202
Xiachoo <odinochkamoto@mail.ru>
2024-11-10 10:38:05 +0300

    Add build script and update render config

--- mysite/build.sh
+++ mysite/build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Exit on error
 set -o errexit
 
-# Установка виртуального окружения и зависимостей
+cd mysite
+
+# Установка зависимостей
 python -m pip install --upgrade pip wheel setuptools
 pip install -r requirements.txt
 
 # Создание необходимых директорий
 python create_static_dirs.py
-
-# Экспорт переменной DJANGO_SETTINGS_MODULE
-export DJANGO_SETTINGS_MODULE=config.settings
 
 # Сбор статических файлов
 python manage.py collectstatic --no-input
