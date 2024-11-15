01684f6ff1a8a90002e2241b3b61914288dba894
Xiachoo <odinochkamoto@mail.ru>
2024-11-15 14:05:39 +0300

    Normalize line endings143д45

--- mysite/build.sh
+++ mysite/build.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Создаем директорию для базы данных
-mkdir -p /opt/render/project/src/mysite
-
-# Если мы на Render и база существует в корне, перемещаем её
-if [ "$RENDER" = "true" ] && [ -f db.sqlite3 ]; then
-    mv db.sqlite3 /opt/render/project/src/mysite/
-fi
-
-# Установка зависимостей
 pip install -r requirements.txt
 
-# Применяем миграции
-python manage.py migrate
-
-# Собираем статические файлы
 python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py create_superuser
