28c7a6624bfadaf64e926e41e404a60751fcb6d7
Xiachoo <odinochkamoto@mail.ru>
2024-11-15 20:19:57 +0300

    Normalize line endings143д4л535

--- mysite/build.sh
+++ mysite/build.sh
@@ -4,13 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Ждем доступности базы данных
-echo "Waiting for PostgreSQL..."
-while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-  sleep 0.1
-done
-echo "PostgreSQL started"
-
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py create_superuser
