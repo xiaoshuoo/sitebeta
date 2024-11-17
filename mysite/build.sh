7c379076fabdeb2974f0fa8b23370b4e9dfa38ab
Xiachoo <odinochkamoto@mail.ru>
2024-11-17 16:38:02 +0300

    Normalize line 4433д4434л3д

--- mysite/build.sh
+++ mysite/build.sh
@@ -5,11 +5,11 @@ set -o errexit
 # Устанавливаем зависимости
 pip install -r requirements.txt
 
-# Создаем необходимые директории в постоянном хранилище
-mkdir -p /opt/render/project/src/data/media/avatars
-mkdir -p /opt/render/project/src/data/media/posts
-mkdir -p /opt/render/project/src/data/media/covers
-mkdir -p /opt/render/project/src/data/static
+# Создаем необходимые директории
+mkdir -p static/css static/js media/avatars media/posts media/covers
+
+# Копируем CSS файлы
+cp -r static/css/* staticfiles/css/ || true
 
 # Применяем миграции
 python manage.py migrate --noinput
@@ -20,5 +20,23 @@ python manage.py createcachetable
 # Создаем суперпользователя
 python manage.py create_superuser
 
-# Собираем статические файлы
-python manage.py collectstatic --no-input --clear
+# Собираем статические файлы без пост-обработки
+DISABLE_COLLECTSTATIC=1 python manage.py collectstatic --no-input --clear --no-post-process --ignore=*.scss --ignore=*.sass --ignore=tailwind.config.js
+
+# Проверяем подключение к базе данных
+python -c "
+import psycopg2
+conn = psycopg2.connect(
+    dbname='django_blog_7f9a',
+    user='django_blog_7f9a_user',
+    password='qNKOalXZlLxzA7rlrYmbkN96ZJ6oHbbE',
+    host='dpg-csrl8f1u0jms7392hlrg-a.oregon-postgres.render.com',
+    port='5432',
+    sslmode='require'
+)
+cursor = conn.cursor()
+cursor.execute('SELECT version();')
+print('Database connection successful:', cursor.fetchone()[0])
+cursor.close()
+conn.close()
+"
