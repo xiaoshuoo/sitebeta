db4a742e96914b19a27e37ec34f037e28a46e569
Xiaoshuoo <136738645+xiaoshuoo@users.noreply.github.com>
2024-12-30 23:00:19 +0300

    first commitд34333д

--- mysite/build.sh
+++ mysite/build.sh
@@ -5,6 +5,15 @@ set -o errexit
 # Install python dependencies
 pip install -r requirements.txt
 
+# Create necessary directories
+mkdir -p staticfiles
+mkdir -p static/css
+mkdir -p static/js
+mkdir -p static/images
+
+# Clean up old static files
+rm -rf staticfiles/*
+
 # Collect static files
 python manage.py collectstatic --no-input
 
