e5eca9b9df0d5b0862d3cf680632f90528a29884
Xiaoshuoo <136738645+xiaoshuoo@users.noreply.github.com>
2024-12-30 20:55:28 +0300

    first commitд34333д31

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -3,6 +3,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
